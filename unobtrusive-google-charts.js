c0d9ac4703857e0905c67c9e774e905e76875925
Craig Curtis <craig.d.curtis@gmail.com>
2017-06-09 15:31:17 +1000

    Create unobtrusive-google-charts.js

---
+++ unobtrusive-google-charts.js
@@ -0,0 +1,107 @@
+$(document).ready(function() {
+    google.charts.load('current', {packages:['geochart', 'table', 'line', 'corechart', 'bar']});
+    google.charts.setOnLoadCallback(drawCharts);
+
+    $('.chartSelect').change(function(){
+        var genre = $(this).val();
+        var chartId = $(this).attr("chart-id");
+        var dataPrefix = $(this).attr("data-prefix");
+        var chartElement = $("#"+chartId)[0];
+
+        $.getJSON(dataPrefix + genre + ".json", function(json) {
+          var data = google.visualization.arrayToDataTable(json);
+          var options = {
+              orientation: "vertical",
+              legend: {position: "none"}
+          };
+          var chart = new google.charts.Bar(chartElement);
+
+          chart.draw(data, google.charts.Bar.convertOptions(options));
+        });
+    });
+
+    $('.geoMap').each(function() {
+        var dataUrl = $(this).attr("data-url");
+        var chartElement = $(this)[0];
+
+        $.getJSON(dataUrl, function(json) {
+          var data = google.visualization.arrayToDataTable(json);
+          var options = {region: 'AU', displayMode: 'markers', colors:['blue', 'red']};
+          var chart = new google.visualization.GeoChart(chartElement);
+
+          chart.draw(data, options);
+        });
+    });
+
+    $('.barChart').each(function() {
+        var data = new google.visualization.DataTable();
+        var dataUrl = $(this).attr("data-url");
+        var chartElement = $(this)[0];
+
+        $.getJSON(dataUrl, function(json) {
+          var data = google.visualization.arrayToDataTable(json);
+          var options = {
+              orientation: "vertical",
+              legend: {position: "none"}
+          };
+          var chart = new google.charts.Bar(chartElement);
+
+          chart.draw(data, google.charts.Bar.convertOptions(options));
+        });
+    });
+
+    $('.pieChart').each(function() {
+        var data = new google.visualization.DataTable();
+        var dataUrl = $(this).attr("data-url");
+        var chartElement = $(this)[0];
+
+        $.getJSON(dataUrl, function(json) {
+          var data = google.visualization.arrayToDataTable(json);
+          var options = {};
+          var chart = new google.visualization.PieChart(chartElement);
+
+          chart.draw(data, options);
+        });
+    });
+
+    $('.lineChart').each(function() {
+        var data = new google.visualization.DataTable();
+        var dataUrl = $(this).attr("data-url");
+        var chartElement = $(this)[0];
+
+        $.getJSON(dataUrl, function(json) {
+          var data = google.visualization.arrayToDataTable(json);
+          var options = {
+              curveType: "function"
+          };
+          var chart = new google.charts.Line(chartElement);
+
+          chart.draw(data, google.charts.Line.convertOptions(options));
+        });
+    });
+
+    $(".table").each(function() {
+        var dataUrl = $(this).attr("data-url");
+        var dataColumns = $(this).attr("data-columns");
+        var tableElement = $(this)[0];
+
+        $.getJSON(dataUrl, function(json) {
+          var data = new google.visualization.DataTable();
+            $.each(JSON.parse(dataColumns), function(i, val) {
+                data.addColumn(val[0], val[1]);
+            });
+
+          data.addRows(json);
+
+          var table = new google.visualization.Table(tableElement);
+          var options = {
+              showRowNumber: false, 
+              width: '100%', 
+              height: '100%',
+              sortColumn: 2,
+              sortAscending: false
+          };
+          table.draw(data, options);
+        });
+    });
+});
