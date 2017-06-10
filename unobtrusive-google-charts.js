4a9362ec356aac52f6603f251b0736cf8c2b51cf
Craig Curtis <craig.d.curtis@gmail.com>
2017-06-10 11:32:49 +1000

    Create unobtrusive-google-charts.js

--- unobtrusive-google-charts.js
+++ unobtrusive-google-charts.js
@@ -1,109 +1,67 @@
-$(document).ready(function() {
-    google.charts.load('current', {packages:['geochart', 'table', 'line', 'corechart', 'bar']});
-    google.charts.setOnLoadCallback(drawCharts);
-
-  function drawCharts() {
-      $('.chartSelect').change(function(){
-          var chartDataUrl = $(this).val();
-          var chartId = $(this).attr("chart-id");
-          var chartElement = $("#"+chartId)[0];
-
-          $.getJSON(chartDataUrl, function(json) {
-            var data = google.visualization.arrayToDataTable(json);
-            var options = {
-                orientation: "vertical",
-                legend: {position: "none"}
-            };
-            var chart = new google.charts.Bar(chartElement);
-
-            chart.draw(data, google.charts.Bar.convertOptions(options));
-          });
-      });
-
-      $('.geoMap').each(function() {
-          var dataUrl = $(this).attr("data-url");
-          var chartElement = $(this)[0];
-          
-          $.getJSON(dataUrl, function(json) {
-            var data = google.visualization.arrayToDataTable(json);
-            var options = {region: 'AU', displayMode: 'markers', colors:['blue', 'red']};
-            var chart = new google.visualization.GeoChart(chartElement);
-
+var unobtrusiveGoogleCharts = {
+    options: {},
+    drawChart: {
+        "geoMap": function (element, data, options) {
+            var chart = new google.visualization.GeoChart(element);
             chart.draw(data, options);
-          });
-      });
-
-      $('.barChart').each(function() {
-          var data = new google.visualization.DataTable();
-          var dataUrl = $(this).attr("data-url");
-          var chartElement = $(this)[0];
-
-          $.getJSON(dataUrl, function(json) {
-            var data = google.visualization.arrayToDataTable(json);
-            var options = {
-                orientation: "vertical",
-                legend: {position: "none"}
-            };
-            var chart = new google.charts.Bar(chartElement);
-
+        },
+        "bar": function (element, data, options) {
+            var chart = new google.charts.Bar(element);
             chart.draw(data, google.charts.Bar.convertOptions(options));
-          });
-      });
-
-      $('.pieChart').each(function() {
-          var data = new google.visualization.DataTable();
-          var dataUrl = $(this).attr("data-url");
-          var chartElement = $(this)[0];
-
-          $.getJSON(dataUrl, function(json) {
-            var data = google.visualization.arrayToDataTable(json);
-            var options = {};
-            var chart = new google.visualization.PieChart(chartElement);
-
+        },
+        "pie": function (element, data, options) {
+            var chart = new google.visualization.PieChart(element);
             chart.draw(data, options);
-          });
-      });
-
-      $('.lineChart').each(function() {
-          var data = new google.visualization.DataTable();
-          var dataUrl = $(this).attr("data-url");
-          var chartElement = $(this)[0];
-
-          $.getJSON(dataUrl, function(json) {
-            var data = google.visualization.arrayToDataTable(json);
-            var options = {
-                curveType: "function"
-            };
-            var chart = new google.charts.Line(chartElement);
-
+        },
+        "line": function (element, data, options) {
+            var chart = new google.charts.Line(element);
             chart.draw(data, google.charts.Line.convertOptions(options));
-          });
-      });
-      
-      $(".table").each(function() {
-          var dataUrl = $(this).attr("data-url");
-          var dataColumns = $(this).attr("data-columns");
-          var tableElement = $(this)[0];
-          
-          $.getJSON(dataUrl, function(json) {
-            var data = new google.visualization.DataTable();
-              $.each(JSON.parse(dataColumns), function(i, val) {
-                  data.addColumn(val[0], val[1]);
-              });
-              
-            data.addRows(json);
-
-            var table = new google.visualization.Table(tableElement);
-            var options = {
-                showRowNumber: false, 
-                width: '100%', 
-                height: '100%',
-                sortColumn: 2,
-                sortAscending: false
-            };
+        },
+        "table": function (element, data, options) {
+            var table = new google.visualization.Table(element);
             table.draw(data, options);
-          });
-      });
-  }
+        }
+    },
 
+    drawChartElement: function (element, data) {
+        var dataUrl = data || element.attr("data");
+        var chartType = element.attr("chart-type");
+        var options = element.data("options") || unobtrusiveGoogleCharts.options[chartType];
+        var dataColumns = element.attr("columns");
+        var chartElement = element[0];
+
+        $.getJSON(dataUrl, function (json) {
+            var data = new google.visualization.DataTable();
+
+            if (chartType === "table") {
+                $.each(JSON.parse(dataColumns), function (i, val) {
+                    data.addColumn(val[0], val[1]);
+                });
+                data.addRows(json);
+            } else {
+                data = google.visualization.arrayToDataTable(json);
+            }
+
+            unobtrusiveGoogleCharts.drawChart[chartType](chartElement, data, options);
+        });
+    },
+
+    bindChartSelectors: function () {
+        $('div.chart').each(function () {
+            unobtrusiveGoogleCharts.drawChartElement($(this));
+        });
+
+        $('.chartSelect').change(function () {
+            var data = $(this).val();
+            var chartId = $(this).attr("chart-id");
+            var chartElement = $("#" + chartId);
+
+            unobtrusiveGoogleCharts.drawChartElement(chartElement, data);
+        });
+    }
+}
+
+$(document).ready(function () {
+    google.charts.load('current', { packages: ['geochart', 'table', 'line', 'corechart', 'bar'] });
+    google.charts.setOnLoadCallback(unobtrusiveGoogleCharts.bindChartSelectors);
 });
